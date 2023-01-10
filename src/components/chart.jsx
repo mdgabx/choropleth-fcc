e9c3d04d71215988123e31d0499f01bf4f30962a
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2023-01-10 18:42:53 +0800

    adding the legend

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -42,6 +42,16 @@ const Chart = (props) => {
                 }
 
             })
+            .attr('data-fips', (countyDataItem) => {
+                return countyDataItem['id']
+            })
+            .attr('data-education', (countyDataItem) => {
+                let id = countyDataItem['id']
+                let county = educData.find((item) => item["fips"] === id)
+                let percentageBachelor = county["bachelorsOrHigher"]
+                return percentageBachelor;
+
+            })
 
             
     }
@@ -51,8 +61,26 @@ const Chart = (props) => {
             <h1 id="title">United States Educational Attainment</h1>
             <p id="description">Adults percentage with a bachelors degree and higher (2010-2014)</p>
             <div className="svg-chart">
-
+              
             </div>
+            <svg id="legend">
+                <g>
+                    <rect x="10" y="0" width="40" height="40" fill="red"></rect>
+                    <text x="60" y="20" fill="white">Less than 15%</text>
+                </g>
+                <g>
+                    <rect x="10" y="40" width="40" height="40" fill="orange"></rect>
+                    <text x="60" y="60" fill="white">between 15% and 30%</text>
+                </g>
+                <g>
+                    <rect x="10" y="80" width="40" height="40" fill="maroon"></rect>
+                    <text x="60" y="100" fill="white">between 30% and 45%</text>
+                </g>
+                <g>
+                    <rect x="10" y="120" width="40" height="40" fill="green"></rect>
+                    <text x="60" y="140" fill="white">greater than 45%</text>
+                </g>
+            </svg>
         </div>
      );
 }
